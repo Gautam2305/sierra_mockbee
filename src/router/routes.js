3518b58bd4612075bbd943aefff0635360c30d91
Gautam Adatra <gautamkiran123@gmail.com>
2022-03-26 17:49:09 +0530

    added: separate folder for all routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,18 +7,20 @@ import { LogoutPage } from "../pages/LogoutPage";
 import { SignupPage } from "../pages/SignupPage";
 import { ProductsListPage } from "../pages/ProductsListPage";
 import { MockAPI } from "../mockman";
+import { ErrorComponent } from "../components/ErrorComponent";
 
-export const Router = () => {
+export const RouterPath = () => {
     return(
         <Routes>
             <Route path="/" element={<LandingPage/>} />
-            <Route path="/productlist" element={<ProductsListPage/>} />
+            <Route path="/products" element={<ProductsListPage/>} />
             <Route path="/wishlist" element={<WishlistPage/>} />
             <Route path="/cart" element={<CartPage/>} />
             <Route path="/login" element={<LoginPage/>} />
             <Route path="/signup" element={<SignupPage/>} />
             <Route path="/logout" element={<LogoutPage/>} />
             <Route path="/mockman" element={<MockAPI/>}/>
+            <Route path="*" element={<ErrorComponent/>}/> 
         </Routes>
     );
 }
