8b816d24aedc01dc420633a9494bbe85a590328c
Gautam Adatra <gautamkiran123@gmail.com>
2022-03-26 17:53:40 +0530

    completed landing page by importing in app & index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
-import {Router} from "./router/routes.js";
+import { CartPage } from "./pages/CartPage";
+import { LandingPage } from "./pages/LandingPage";
+import { LogoutPage } from "./pages/LogoutPage";
+import { WishlistPage } from "./pages/WishlistPage";
+import { RouterPath } from "./router/routes.js";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Router/>
+      <RouterPath>
+      <LandingPage></LandingPage> 
+      <WishlistPage></WishlistPage>
+      <CartPage></CartPage>
+      <LogoutPage></LogoutPage>
+      </RouterPath> 
     </div>
   );
 }
