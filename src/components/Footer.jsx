94fe50c9c45d3ec49d488ca373896a50e014f44f
Gautam Adatra <gautamkiran123@gmail.com>
2022-05-21 06:14:05 +0530

    FIX: minor changes in files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,7 @@ export const Footer = () =>{
              <footer className="main-foot">
             <div className="brand-name"><h3>SIERRA</h3></div>
             <div className="auth-info">
-                <h4>ONLINE SHOPPING</h4>
-                <p>Men</p>
-                <p>Women</p>
-                <p>Kids</p>
-                <p>Beauty</p>
-                <p>Gift Cards</p>
+                <h4>Made by GAUTAM with love</h4>
             </div>
             <div className="auth-link">
                 <a target="_blank" href="https://github.com/Gautam2305" className="social-link github-link"><i className="fa-brands fa-github"></i></a>
