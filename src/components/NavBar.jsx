0375f887198e060d56eb3384f09a33c64170a03a
Gautam Adatra <gautamkiran123@gmail.com>
2022-09-11 00:42:59 +0530

    FIX: nav login button to change dynamically

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"; 
+import { useAuth } from "../contexts/auth-context";
 export const NavBar = () =>{
+    const { user } = useAuth();
     return(
         <div>
             <nav className="main-nav">
@@ -8,7 +10,7 @@ export const NavBar = () =>{
                 </div>
                 <input className="nav-search" type="text" placeholder="Search for products, brands & more..."/>
                 <div className="nav-icons">
-                    <Link to="/logout"><button className="btn-primary-solid link-primary-solid"><h4>LOGOUT</h4></button></Link>
+                    <Link to="/login"><button className="btn-primary-solid link-primary-solid"><h4>{ user.token === null ? "LOGIN" : "LOGOUT"}</h4></button></Link>
                     <Link to="/wishlist" className="btn-icon wishlist-icon"><i className="fa-regular fa-heart"></i><h6> Wishlist</h6></Link>
                     <Link to="/cart" className="btn-icon bag-icon "><i className="fa-solid fa-bag-shopping"></i><h6> Bag</h6></Link>
                 </div>
