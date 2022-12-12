e486059e0d366535c72515db8b6d54ccce9312bc
Gautam Adatra <gautamkiran123@gmail.com>
2022-12-12 23:32:07 +0530

    feat: cart context

---
+++ src/contexts/cart-context.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "istanbul-lib-report";
+import React, { useContext, useState } from "react";
+
+const cartContext = createContext();
+
+const CartProvider = ({children}) => {
+    const [cart, setCart] = useState({ cart: []});
+    return(
+        <cartContext.Provider value={{cart,setCart}}>
+            {children}
+        </cartContext.Provider>
+    );
+}
+
+const useCart = () => useContext(cartContext);
+
+export { CartProvider,useCart};
